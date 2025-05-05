c51d1b1c9d3ef770c33ca70252b8d8177291c853
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2025-05-05 23:36:31 +0300

    flatten bt

---
+++ BIN-TREE/flatten-to-linked-list-preorder.ts
@@ -0,0 +1,58 @@
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// N time
+// N space
+
+function flattendfs(root: TreeNode | null): void {
+  // flatten root subtree and return its tail
+  function dfs(root: TreeNode | null): TreeNode | null {
+    if (!root) return null;
+
+    const leftSubtreeTail = dfs(root.left);
+    const rightSubtreeTail = dfs(root.right);
+
+    // only if left subtree not empty we need to do
+    // anything
+    if (leftSubtreeTail) {
+      leftSubtreeTail.right = root.right;
+      root.right = root.left;
+      root.left = null;
+    }
+
+    // if here were no rightsubtree return left
+    // or the root itself if none present
+    return rightSubtreeTail || leftSubtreeTail || root;
+  }
+
+  dfs(root);
+}
+
+// N time
+// 1 space
+
+function flatten(root: TreeNode | null): void {
+  let cur = root;
+
+  while (cur) {
+    if (cur.left) {
+      let prev = cur.left;
+      while (prev.right) {
+        prev = prev.right;
+      }
+      prev.right = cur.right;
+      cur.right = cur.left;
+      cur.left = null;
+    }
+
+    cur = cur.right;
+  }
+}
